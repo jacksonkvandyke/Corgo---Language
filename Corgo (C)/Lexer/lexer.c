35e9da7f3c51167b5e9f689325cee60297df19ae
jacksonkvandyke <jacksonkvandyke@gmail.com>
2026-01-01 13:18:23 -0600

    Made some more progress on the lexer.

--- Corgo (C)/Lexer/lexer.c
+++ Corgo (C)/Lexer/lexer.c
@@ -6,6 +6,7 @@
 //C Includes
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 
 //MY Includes
@@ -32,6 +33,8 @@ int LexAdvance(struct Lexer *lexer) {
     } else {
         lexer->current_char = lexer->source[lexer->pos];
     }
+
+    return 0;
 }
 
 //Lex-Peek - Look at the next character without incrementing the lexer.
@@ -44,14 +47,78 @@ char LexPeek (struct Lexer *lexer){
     }
 }
 
+//Lex-SkipWhitespace - Skip all whitespace.
+int LexSkipWhitespace (struct Lexer *lexer) {
+    while ((isspace(lexer->current_char)) && (lexer->current_char != '\0')) {
+        LexAdvance(lexer);
+    }
 
+    return 0;
+}
+
+//Lex-IdentifierORKeyword - Get an identifier or keyword.
+
+
+//Lex-GetNextToken - Get the next token from source.
+Token LexGetNextToken(struct Lexer *lexer) {
+    while (lexer->current_char != '\0'){
+        //Check for new-line
+        if (lexer->current_char == '\n') {
+            //Create the token
+            Token token;
+            token.type = NEWLINE;
+            token.start = lexer->source + lexer->pos;
+            token.length = 1;
+            token.line = lexer->line;
+            token.column = lexer->column;
+
+            //Advance the lexer
+            LexAdvance(lexer);
+
+            //Return the token
+            return token;
+        }
+
+        //Check for whitespace
+        if (isspace(lexer->current_char)) {
+            LexSkipWhitespace(lexer);
+            continue;
+        }
+
+        //Check for arrow notation
+        if ((lexer->current_char == '-') && (LexPeek(lexer) == '>')) {
+            //Create the token
+            Token token;
+            token.type = OPERATOR_ARROW;
+            token.start = lexer->source + lexer->pos;
+            token.length = 2;
+            token.line = lexer->line;
+            token.column = lexer->column;
+
+            //Advance the lexer 2 since we absorbed 2 characters
+            LexAdvance(lexer);
+            LexAdvance(lexer);
+
+            //Return the token
+            return token;
+        }
+
+        //Check for alpha characters
+        if (isalpha(lexer->current_char)) {
+
+        }
+
+        ////TEST ADVANCE
+        LexAdvance(lexer);
+    }
+}
 
 
 //Test main
 int main(void) {
     struct Lexer lexer;
 
-    lexer.source = "Test string value.";
+    lexer.source = "Test string value.\n";
     lexer.length = strlen(lexer.source);
     lexer.pos = 0;
     lexer.current_char = lexer.source[lexer.pos];
@@ -59,8 +126,8 @@ int main(void) {
     lexer.column = 0;
 
     while (lexer.current_char != '\0') {
-        printf("%c", lexer.current_char);
-        LexAdvance(&lexer);
+        Token token = LexGetNextToken(&lexer);
+        printf("%d", token.type);
     }
 
     return 0;
